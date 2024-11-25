f9ad51130a0ce84f977e1fb67a870b6891159552
ilyesb36 <ilyesb36130@gmail.com>
2024-11-25 15:44:24 +0100

    add base back

---
+++ back/src/models/Tache.ts
@@ -0,0 +1,76 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../config/database';
+import Project from './Project';
+
+interface TacheAttributes {
+  id: number;
+  titre: string;
+  description: string;
+  date_creation: Date;
+  date_echeance: Date;
+  priorite: 'basse' | 'moyenne' | 'haute';
+  statut: 'à faire' | 'en cours' | 'terminée';
+  project_id: number;
+}
+
+interface TacheCreationAttributes extends Optional<TacheAttributes, 'id'> {}
+
+class Tache extends Model<TacheAttributes, TacheCreationAttributes> implements TacheAttributes {
+  public id!: number;
+  public titre!: string;
+  public description!: string;
+  public date_creation!: Date;
+  public date_echeance!: Date;
+  public priorite!: 'basse' | 'moyenne' | 'haute';
+  public statut!: 'à faire' | 'en cours' | 'terminée';
+  public project_id!: number;
+}
+
+Tache.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    titre: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_creation: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    date_echeance: {
+      type: DataTypes.DATEONLY,
+    },
+    priorite: {
+      type: DataTypes.ENUM('basse', 'moyenne', 'haute'),
+      defaultValue: 'moyenne',
+    },
+    statut: {
+      type: DataTypes.ENUM('à faire', 'en cours', 'terminée'),
+      defaultValue: 'à faire',
+    },
+    project_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Project,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Tache',
+  }
+);
+
+Tache.belongsTo(Project, { foreignKey: 'project_id', onDelete: 'CASCADE' });
+
+Tache.sync();
+
+export default Tache;
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,13 +1,38 @@
 import express from "express";
-import authRoutes from "./modules/auth/auth.routes"; // Importation des routes
-import { Request, Response } from "express";
+import authRoutes from "./modules/auth/auth.routes";
+import sequelize from "./config/database";
+import router from "./routes/routes";
+
+import User from "./models/User";
+import Tache from "./models/Tache";
+import Tag from "./models/Tag";
+import UsersTaches from "./models/UsersTaches";
+import TachesTags from "./models/TachesTags";
+import Project from "./models/Project";
+import UsersProjects from "./models/UsersProjects";
+
 
 const app = express();
 app.use(express.json());
 
+app.use(router);
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    await User.sync();
+    await Project.sync();
+    await Tache.sync();
+    await Tag.sync();
+    await UsersTaches.sync();
+    await TachesTags.sync();
+    await UsersProjects.sync();
+
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database");
+  }
+  await sequelize.sync();  console.log(`Server is running on http://localhost:${PORT}`);
 });
