3ae2c36c9c1f1c5685d961b33ae4f1f9652da31b
FelixMillon <felix.millon@outlook.com>
2024-11-26 13:45:20 +0100

    add cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import authRoutes from "./modules/auth/auth.routes";
 import sequelize from "./config/database";
 import router from "./routes/routes";
-
+import cors from "cors";
 import User from "./models/User";
 import Tache from "./models/Tache";
 import Tag from "./models/Tag";
@@ -13,6 +13,11 @@ import UsersProjects from "./models/UsersProjects";
 
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost/*", // Autoriser seulement cette origine
+  methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes autorisées
+  allowedHeaders: ["Content-Type", "Authorization"], // Headers autorisés
+}));
 app.use(express.json());
 
 app.use(router);
