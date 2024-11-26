1b8c91101b4c50debdf11cc82ae12272dd04bd98
FelixMillon <felix.millon@outlook.com>
2024-11-26 16:10:03 +0100

    uniformize auth and notify

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./modules/auth/auth.routes";
 import { errorHandler } from "./utils/errorHandler";
 
 const app = express();
--- back/src/modules/notify/notify.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { notifyController } from "./notify.controller";
-
-const router = Router();
-
-router.post("/", notifyController.send);
-export default router;
