a8c8eeae542276c93bcdfe8642ededc2b16e8256
Elone Maccioni <elonemacc@gmail.com>
2024-11-28 13:30:37 +0100

    addUser HomePage after login

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -7,7 +7,32 @@ const HomePage: React.FC = () => {
   const [tasks, setTasks] = useState<any[]>([]);
   const [filteredTasks, setFilteredTasks] = useState<any[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string | null>(null);
+
+  // Fonction pour récupérer les informations de l'utilisateur connecté
+  const fetchUserInfo = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        console.error('Aucun token trouvé, redirection vers la page de connexion.');
+        return;
+      }
+
+      const response = await axios.get('http://localhost:3000/auth/verify', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200 && response.data.user && response.data.user.nom) {
+        setUserName(response.data.user.nom); // Récupérer le nom de l'utilisateur
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des informations de l\'utilisateur:', error);
+      alert('Impossible de récupérer les informations de l\'utilisateur. Veuillez vous reconnecter.');
+    }
+  };
 
   const fetchTasks = async () => {
     try {
@@ -21,8 +46,7 @@ const HomePage: React.FC = () => {
 
   useEffect(() => {
     fetchTasks();
-    const email = localStorage.getItem('userEmail');
-    setUserEmail(email);
+    fetchUserInfo(); // Récupérer les informations de l'utilisateur à chaque chargement de la page
   }, []);
 
   useEffect(() => {
@@ -39,18 +63,18 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="home-page">
-      <h1>Bienvenue {userEmail ? userEmail : "Utilisateur"}</h1>
+      <h1>Bienvenue <span className='user'>{userName ? userName : "Utilisateur"}</span></h1>
       <div className='div'>
         <Link to="/add-task" className="add-task-button">Ajouter une nouvelle tâche</Link>
         <div>
           <label>Faire une recherche :</label>
-         <input
-          type="text"
-          placeholder="Rechercher des tâches..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="search-bar"
-      />
+          <input
+            type="text"
+            placeholder="Rechercher des tâches..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="search-bar"
+          />
         </div>
       </div>
       <p>Voici la liste de toutes vos tâches :</p>
